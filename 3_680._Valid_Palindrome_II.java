e98ae8c24805d8d5c08b424555a25dca709f399e
Sumit Borhade <sumitborhade@gmail.com>
2021-09-14 00:06:26 +0800

    Create 3_680._Valid_Palindrome_II.java

---
+++ 3_680._Valid_Palindrome_II.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        if(s == null || s.length() == 0) {
+            return true;
+        }
+        
+        int start = 0;
+        int end = s.length()-1;
+        
+        while(start < end) {
+            if(s.charAt(start) != s.charAt(end)) {
+                return isPalindrome(s, start, end-1) 
+                    || isPalindrome(s, start+1, end);
+                
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    
+    public boolean isPalindrome(String s, int start, int end) {
+        while(start < end) {
+            if(s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+}
