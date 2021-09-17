a29c013644535d0efbcb2c49e1e0ad5390ae7957
Sumit Borhade <sumitborhade@gmail.com>
2021-09-18 01:13:54 +0800

    Create 17_215._Kth_Largest_Element_in_an_Array.java

---
+++ 17_215._Kth_Largest_Element_in_an_Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int num : nums) {
+            pq.offer(num);
+            
+            if(pq.size() > k) {
+                pq.poll();
+            }
+        }
+        
+        return pq.peek();
+    }
+}
